cb5a92fe5fe433d67bd3bde65cce3003bbd2cf89
yousef5 <yousef311@hotmail.com>
2025-04-20 13:39:31 +0200

    Update Next.js configuration for GitHub Pages deployment and adjust image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/healthy-web" : "",
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
