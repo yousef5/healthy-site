0e433782424bfff3e83432282896fe472e7e7d1c
yousef5 <yousef311@hotmail.com>
2025-04-20 13:49:14 +0200

    Update Next.js configuration to conditionally set output for GitHub Actions and enable environment variable for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  output: process.env.GITHUB_ACTIONS === "true" ? "export" : undefined,
   images: {
     unoptimized: true,
   },
