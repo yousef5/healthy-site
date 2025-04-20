da0fe80bacb41d33d21351324bcc8f3933451cc5
yousef5 <yousef311@hotmail.com>
2025-04-20 13:51:11 +0200

    Refactor robots.txt and sitemap.ts to use MetadataRoute for improved SEO handling and enforce static generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://next-app-i18n-starter.vercel.app";
   const locales = ["en", "ar", "zh", "es", "jp"] as const;
